0643412fd678ec90e619699309f6c0bb0f7337f9
Isaac Martin <45830500+isaacmartin1@users.noreply.github.com>
2026-04-08 00:31:02 -0400

    Add: validate-parentheses - submission-21

---
+++ Data Structures & Algorithms/validate-parentheses/submission-21.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = []
+        b = "({["
+
+        bracket_dict = {
+            '}': '{',
+            ']': '[',
+            ')': '('
+        }
+
+        for i in range(len(s)):
+            print(brackets)
+            if s[i] in b:
+                brackets.append(s[i])
+            else:
+                if len(brackets) == 0:
+                    return False
+
+                if bracket_dict[s[i]] != brackets.pop():
+                    return False
+        
+        if len(brackets) > 0:
+            return False
+        return True
