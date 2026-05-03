0979e8de830f4a6da86be63cc12a6f3722fbf24c
Isaac Martin <45830500+isaacmartin1@users.noreply.github.com>
2026-05-02 20:18:01 -0400

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+
+        target_dict = defaultdict(int)
+
+        for i in range(len(s1)):
+            target_dict[s1[i]] += 1
+
+        l = 0
+        print(target_dict)
+
+        for r in range(len(s2)):
+            if r - l + 1 > len(s1):
+                l += 1
+            
+
+            iteration_copy = target_dict.copy()
+
+            for i in range(l, r + 1):
+                curr_key = s2[i]
+                iteration_copy[curr_key] -= 1
+            
+            match_found = True
+            for val in iteration_copy.values():
+                if val != 0:
+                    match_found = False
+
+            if match_found:
+                return True
+
+            r += 1
+        return False
