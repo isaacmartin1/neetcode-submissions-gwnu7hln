9129eca5dba5281da50c1746c4a1b207321ab77a
Isaac Martin <45830500+isaacmartin1@users.noreply.github.com>
2026-05-02 20:25:19 -0400

    Add: permutation-string - submission-19

---
+++ Data Structures & Algorithms/permutation-string/submission-19.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+
+        target_dict = defaultdict(int)
+
+        for i in range(len(s1)):
+            target_dict[s1[i]] += 1
+
+        l = 0
+
+        for r in range(len(s2)):
+            if r - l + 1 > len(s1):
+                l += 1
+
+
+            if sorted(s1) == sorted(s2[l: r + 1]):
+                return True
+
+            r += 1
+        return False
